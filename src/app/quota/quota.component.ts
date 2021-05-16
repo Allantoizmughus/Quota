16bf52e505b6b7700bfc709f48d467e718b2a6ee
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-05-16 15:33:32 +0300

    Add some quotes in using the quote logic

--- src/app/quota/quota.component.ts
+++ src/app/quota/quota.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quota',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quota.component.css']
 })
 export class QuotaComponent implements OnInit {
+  quotes:Quote[]=[
+    new Quote('Life is what  you make it', 'Quote by Allan.M'),
+    new Quote('Love is a Jouurney', 'Quote by sheng peng'),
+    new Quote('You Only Live Once', 'Quote by Bad Thoughts'),
+    new Quote('What goes around...', '  Quote by Ash Tray'),
+    new Quote('Karma is a B****', 'Quote by Bad Thoughts')
+    
+  ];
 
   constructor() { }
 
